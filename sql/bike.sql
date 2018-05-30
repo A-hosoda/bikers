1fdd005a0d44232281dc32135cdbd50a7d0fde1f
A-hosoda <robosol3734fullmetal@yahoo.co.jp>
2018-05-30 13:12:57 +0900

    SQL更新

--- sql/bike.sql
+++ sql/bike.sql
@@ -54,7 +54,7 @@ create table rindo (
 );
 
 INSERT INTO userLoginData (userName,userPassword,userAge,userMail,userGender) VALUES ("hosoda","123",26,"hosoda@aaaaaa","男");
-INSERT INTO userLoginData (userName,userPassword,userAge,userMail,userGender) VALUES ("さくらい","123",26,"sakurai@aaaaaa","男");
+INSERT INTO userLoginData (userName,userPassword,userAge,userMail,userGender) VALUES ("sakurai","123",26,"sakurai@aaaaaa","男");
 
 INSERT INTO rindo (rindoName,rindoPlaceName,difficulty,comment,latitude,longitude,img1) VALUES ("スーパー林道天龍","中部地方",2,"初心者でも楽しめる林道。フラットダートで見晴らしがよく、比較的安全です。山頂からの眺めは格別です。","34.9473399","137.8347025","../img/rindoImg1.jpg");
 INSERT INTO rindo (rindoName,rindoPlaceName,difficulty,comment,latitude,longitude,img1) VALUES ("山道","四国地方",1,"四国の酷道。","33.7833276","133.5223045","../img/rindoImg2.jpg");
@@ -62,8 +62,8 @@ INSERT INTO rindo (rindoName,rindoPlaceName,difficulty,comment,latitude,longitud
 
 INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう", "2019/07/21",1,"スーパー林道天龍","hosoda",1,4,1,"楽しもう");
 INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう", "2017/07/21",1,"スーパー林道天龍","hosoda",1,4,1,"楽しもう");
-INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう3", "2018/08/21",1,"スーパー林道天龍","さくらい",2,4,1,"楽しもう");
+INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう3", "2018/08/21",1,"スーパー林道天龍","sakurai",2,4,1,"楽しもう");
 INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう4", "2018/06/10",1,"スーパー林道天龍","hosoda",1,4,1,"楽しもう");
 INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう5", "2018/01/21",2,"山道","hosoda",1,4,1,"楽しもう");
-INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう6", "2018/03/21",2,"山道","さくらい",2,4,1,"楽しもう");
+INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう6", "2018/03/21",2,"山道","sakurai",2,4,1,"楽しもう");
 INSERT INTO ivent (iventName,iventDate,rindo_id,rindoName,sponsor,sponsor_id,maxPeople,currentPeople,comment) VALUES ("林道楽しもう7", "2018/07/21",2,"山道","hosoda",1,4,1,"楽しもう");
